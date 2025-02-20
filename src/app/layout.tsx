cb987e4b16b3851104e8ef8a970a4aaa58c86176
jamie 🌨️ <jamie@wisp.bio>
2025-02-20 12:11:34 -0500

    feat: add utils, edit theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,23 +2,25 @@ import { Providers } from "@/components/providers";
 import "@/styles/globals.css";
 
 import { type Metadata } from "next";
-import { Spline_Sans } from "next/font/google";
+import { IBM_Plex_Sans } from "next/font/google";
+
 export const metadata: Metadata = {
   title: "ui-kit",
   description: "personal ui kit",
   icons: [{ rel: "icon", url: "https://github.com/jvxz.png" }],
 };
 
-const font = Spline_Sans({
+const font = IBM_Plex_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
+  variable: "--font-sans",
 });
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${font.className}`}>
+    <html lang="en" className={`${font.variable}`}>
       <Providers><body>{children}</body></Providers>
     </html>
   );
