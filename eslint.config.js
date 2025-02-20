2b7558ca36be9ca6e87b25748a0d049fe57938db
jamie 🌨️ <jamie@wisp.bio>
2025-02-20 15:52:02 -0500

    chore: remove prettier, update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,26 @@
-import antfu from '@antfu/eslint-config'
+import eslintJs from "@eslint/js";
+import eslintReact from "@eslint-react/eslint-plugin";
+import tseslint from "typescript-eslint";
+import stylistic from '@stylistic/eslint-plugin';
 
-export default antfu({
-  react: true,
-  typescript: true,
-
-  rules: {
-    'unused-imports/no-unused-vars': 'error',
+export default tseslint.config({
+  files: ["**/*.ts", "**/*.tsx"],
+  extends: [
+    eslintJs.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    eslintReact.configs["recommended-type-checked"],
+    stylistic.configs.recommended,
+  ],
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      projectService: true,
+    },
   },
-})
+  rules: {
+    // Put rules you want to override here
+    "@eslint-react/prefer-shorthand-boolean": "warn",
+    '@stylistic/indent': ['error', 2],
+
+  },
+});
