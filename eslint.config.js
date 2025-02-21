4521752aca647fe8bc9510a25ccf1e33e87bc4da
jamie 🌨️ <jamie@wisp.bio>
2025-02-20 20:09:28 -0500

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,25 +2,65 @@ import eslintJs from "@eslint/js";
 import eslintReact from "@eslint-react/eslint-plugin";
 import tseslint from "typescript-eslint";
 import stylistic from '@stylistic/eslint-plugin';
+// @ts-ignore
+import importPlugin from 'eslint-plugin-import';
 
-export default tseslint.config({
-  files: ["**/*.ts", "**/*.tsx"],
-  extends: [
-    eslintJs.configs.recommended,
-    tseslint.configs.recommendedTypeChecked,
-    eslintReact.configs["recommended-type-checked"],
-    stylistic.configs.recommended,
-  ],
-  languageOptions: {
-    parser: tseslint.parser,
-    parserOptions: {
-      projectService: true,
+export default [
+  {
+    ignores: [
+      "**/node_modules/**/*",
+      "**/.next/**/*",
+      "node_modules/**",
+      ".next/**",
+      "dist/**",
+      "build/**"
+    ],
+  },
+
+  ...tseslint.config({
+    files: ["**/*.ts", "**/*.tsx"],
+    extends: [
+      eslintJs.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      eslintReact.configs["recommended-type-checked"],
+      stylistic.configs.recommended,
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+    ],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+      },
     },
-  },
-  rules: {
-    // Put rules you want to override here
-    "@eslint-react/prefer-shorthand-boolean": "warn",
-    '@stylistic/indent': ['error', 2],
+    rules: {
+      "@eslint-react/prefer-shorthand-boolean": "warn",
+      "@eslint-react/no-array-index-key": "error",
+      "@eslint-react/no-children-prop": "error",
+      "@eslint-react/no-duplicate-jsx-props": "error",
+      "@eslint-react/no-leaked-conditional-rendering": "error",
+      "@eslint-react/no-implicit-key": "error",
+      "@eslint-react/no-useless-fragment": "warn",
+      "@eslint-react/hooks-extra/no-unnecessary-use-callback": "warn",
+      "@eslint-react/hooks-extra/no-unnecessary-use-memo": "warn",
 
-  },
-});
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/jsx-closing-bracket-location': [1, 'props-aligned'],
+      '@stylistic/jsx-closing-tag-location': [1, 'line-aligned'],
+      "@stylistic/jsx-one-expression-per-line": ["error", { "allow": "non-jsx" }],
+
+      '@stylistic/space-in-parens': ['error', 'never'],
+
+
+      'import/newline-after-import': 'error',
+      'import/no-unresolved': 'off',
+      'import/order': ['error', {
+        groups: ['builtin', 'external', 'internal'],
+        'newlines-between': 'always',
+        alphabetize: {
+          
+        }
+      }],
+    },
+  }),
+];
