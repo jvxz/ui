7a5a8c49e373d3dd3d6d8ef735b19971e3b27c89
jamie 🌨️ <jamie@wisp.bio>
2025-02-21 18:57:37 -0500

    chore: edit eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -45,7 +45,19 @@ export default [
       "@eslint-react/no-useless-fragment": "warn",
       "@eslint-react/hooks-extra/no-unnecessary-use-callback": "warn",
       "@eslint-react/hooks-extra/no-unnecessary-use-memo": "warn",
+      "@stylistic/object-property-newline": "error",
+      "@stylistic/multiline-ternary": ["error", "always-multiline"],
+      "@stylistic/padding-line-between-statements": [
+        "error",
+        { blankLine: "always", prev: "var", next: "return" }
+      ],
       "@eslint-react/dom/no-unknown-property": ["error", { requireDataLowercase: true }],
+      "@stylistic/object-curly-newline": ["error", {
+        "ObjectExpression": "always",
+        "ObjectPattern": { "multiline": true },
+        "ImportDeclaration": "never",
+        "ExportDeclaration": { "multiline": true, "minProperties": 3 }
+      }],
 
       '@stylistic/indent': ['error', 2],
       '@stylistic/jsx-closing-bracket-location': [1, 'line-aligned'],
@@ -67,6 +79,8 @@ export default [
           caseInsensitive: true,
         }
       }],
+
+
     },
   }),
 ];
