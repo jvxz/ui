47bdfc8582d538555bfe5a16a7e406b5c87c79e0
jamie 🌨️ <jamie@wisp.bio>
2025-02-21 12:52:40 -0500

    refactor: update dropdown menu

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -1,18 +1,19 @@
-import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
 import { Icon } from '@iconify/react'
+import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
 
 import { cn } from '@/lib/utils'
 
 const contentClasses = cn(
-  'bg-popover p-2 border rounded min-w-48 select-none space-y-1 motion-duration-150',
+  'bg-popover p-2 border rounded min-w-48 select-none space-y-1 motion-duration-150 shadow',
   'radix-side-bottom:motion-preset-slide-down-sm',
   'radix-side-top:motion-preset-slide-up-sm',
   'radix-side-left:motion-preset-slide-left-sm',
   'radix-side-right:motion-preset-slide-right-sm',
 )
-function Content({ sideOffset = 6, ...props }: DropdownMenuPrimitive.DropdownMenuContentProps) {
+function Content({ sideOffset = 6, ref, ...props }: DropdownMenuPrimitive.DropdownMenuContentProps & { ref?: React.Ref<HTMLDivElement> }) {
   return (
     <DropdownMenuPrimitive.Content
+      ref={ref}
       className={cn(
         contentClasses,
         props.className)}
@@ -24,13 +25,14 @@ function Content({ sideOffset = 6, ...props }: DropdownMenuPrimitive.DropdownMen
 }
 
 const itemClasses = cn(
-  'py-1 px-2 rounded transition-all duration-100 outline-none flex items-center gap-2',
+  'px-2 py-1 text-popover-foreground rounded transition-all duration-100 outline-none flex items-center gap-2',
   'hover:bg-accent',
   'focus-visible:bg-accent focus-visible:ring',
 )
-function Item({ ...props }: DropdownMenuPrimitive.DropdownMenuItemProps) {
+function Item({ ref, ...props }: DropdownMenuPrimitive.DropdownMenuItemProps & { ref?: React.Ref<HTMLDivElement> }) {
   return (
     <DropdownMenuPrimitive.Item
+      ref={ref}
       className={cn(
         itemClasses,
         props.className)}
@@ -42,9 +44,10 @@ function Item({ ...props }: DropdownMenuPrimitive.DropdownMenuItemProps) {
 const labelClasses = cn(
   'px-2 py-1 font-medium text-muted-foreground text-sm whitespace-nowrap ',
 )
-function Label({ ...props }: DropdownMenuPrimitive.DropdownMenuLabelProps) {
+function Label({ ref, ...props }: DropdownMenuPrimitive.DropdownMenuLabelProps & { ref?: React.Ref<HTMLDivElement> }) {
   return (
     <DropdownMenuPrimitive.Label
+      ref={ref}
       className={cn(
         labelClasses,
         props.className)}
@@ -56,24 +59,29 @@ function Label({ ...props }: DropdownMenuPrimitive.DropdownMenuLabelProps) {
 const radioGroupClasses = cn(
   'flex flex-col gap-1',
 )
-function RadioGroup({ ...props }: DropdownMenuPrimitive.DropdownMenuRadioGroupProps) {
+function RadioGroup({ ref, ...props }: DropdownMenuPrimitive.DropdownMenuRadioGroupProps & { ref?: React.Ref<HTMLDivElement> }) {
   return (
-    <DropdownMenuPrimitive.RadioGroup className={cn(radioGroupClasses, props.className)} {...props} />
+    <DropdownMenuPrimitive.RadioGroup
+      ref={ref}
+      className={cn(radioGroupClasses, props.className)}
+      {...props}
+    />
   )
 }
 
 const radioItemClasses = cn(
   'py-1 px-2 rounded transition-all duration-100 outline-none flex items-center justify-between',
 )
-function RadioItem({ ...props }: DropdownMenuPrimitive.DropdownMenuRadioItemProps) {
+function RadioItem({ ref, ...props }: DropdownMenuPrimitive.DropdownMenuRadioItemProps & { ref?: React.Ref<HTMLDivElement> }) {
   return (
     <DropdownMenuPrimitive.RadioItem
+      ref={ref}
       className={cn(radioItemClasses, itemClasses, props.className)}
       {...props}
     >
       {props.children}
-      <DropdownMenuPrimitive.ItemIndicator>
-        <Icon icon="ph:check" />
+      <DropdownMenuPrimitive.ItemIndicator className="">
+        <Icon icon="ph:radio-button-fill" />
       </DropdownMenuPrimitive.ItemIndicator>
 
     </DropdownMenuPrimitive.RadioItem>
@@ -82,9 +90,10 @@ function RadioItem({ ...props }: DropdownMenuPrimitive.DropdownMenuRadioItemProp
 const separatorClasses = cn(
   'my-2 bg-border h-px',
 )
-function Separator({ ...props }: DropdownMenuPrimitive.DropdownMenuSeparatorProps) {
+function Separator({ ref, ...props }: DropdownMenuPrimitive.DropdownMenuSeparatorProps & { ref?: React.Ref<HTMLDivElement> }) {
   return (
     <DropdownMenuPrimitive.Separator
+      ref={ref}
       className={cn(
         separatorClasses,
         props.className)}
@@ -96,28 +105,34 @@ function Separator({ ...props }: DropdownMenuPrimitive.DropdownMenuSeparatorProp
 const subContentClasses = cn(
   '',
 )
-function SubContent({ ...props }: DropdownMenuPrimitive.DropdownMenuSubContentProps) {
+function SubContent({ ref, ...props }: DropdownMenuPrimitive.DropdownMenuSubContentProps & { ref?: React.Ref<HTMLDivElement> }) {
   return (
-    <DropdownMenuPrimitive.SubContent className={cn(subContentClasses, contentClasses, props.className)} {...props} />
+    <DropdownMenuPrimitive.SubContent
+      ref={ref}
+      className={cn(subContentClasses, contentClasses, props.className)}
+      {...props}
+    />
   )
 }
 
 const subTriggerClasses = cn(
-  '',
+  'flex items-center justify-between',
 )
-function SubTrigger({ children, ...props }: { children: React.ReactNode } & DropdownMenuPrimitive.DropdownMenuSubTriggerProps) {
+function SubTrigger({ children, ref, ...props }: { children: React.ReactNode, ref?: React.Ref<HTMLDivElement> } & DropdownMenuPrimitive.DropdownMenuSubTriggerProps) {
   return (
     <DropdownMenuPrimitive.SubTrigger
+      ref={ref}
       className={cn(
         subTriggerClasses,
         itemClasses,
         props.className)}
       {...props}
     >
-      <div className="flex justify-between items-center [&>div]:p-0">
-        {children}
-        <Icon icon="mdi:chevron-right" />
-      </div>
+      {children}
+      <Icon
+        icon="ph:caret-right-bold"
+        className="ml-auto"
+      />
     </DropdownMenuPrimitive.SubTrigger>
   )
 }
