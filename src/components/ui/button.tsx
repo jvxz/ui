09506b675a09a27e5d949d70ba8bf75c4e9a8f67
jamie 🌨️ <jamie@wisp.bio>
2025-02-20 20:09:46 -0500

    refactor: update button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,28 +1,29 @@
 import type { VariantProps } from 'class-variance-authority'
-import { cn } from '@/lib/utils'
 import { Slot } from '@radix-ui/react-slot'
 import { cva } from 'class-variance-authority'
 import { forwardRef } from 'react'
 
+import { cn } from '@/lib/utils'
+
 const buttonVariants = cva(
-  'flex justify-center items-center gap-2 disabled:opacity-50 rounded font-extralight transition-all duration-100 cursor-pointer disabled:cursor-not-allowed',
+  'flex justify-center items-center gap-2 disabled:opacity-50 rounded outline-0 focus:ring ring-ring font-extralight transition-all duration-100 cursor-pointer disabled:cursor-not-allowed select-none',
   {
     variants: {
       variant: {
         default:
          'border bg-secondary/80 hover:bg-secondary not-disabled:active:border-secondary-foreground/10 not-disabled:active:bg-secondary/80 not-disabled:hover:border-secondary-foreground/20 text-secondary-foreground ',
         destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90 not-disabled:active:bg-destructive/80 ',
+         'bg-destructive text-destructive-foreground hover:bg-destructive/90 not-disabled:active:bg-destructive/80 ',
         outline:
-          'border border-input bg-background hover:bg-accent/50 hover:border-accent-foreground/10 not-disabled:active:bg-accent/40 hover:text-accent-foreground',
+         'border border-input bg-background hover:bg-accent/50 hover:border-accent-foreground/10 not-disabled:active:bg-accent/40 hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground  hover:bg-secondary/80',
         ghost: 'hover:bg-accent/50 hover:text-accent-foreground not-disabled:active:bg-accent/40',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'shadow-lg px-3 py-1 text-sm',
-        sm: 'shadow-lg px-2 py-[2px] text-xs',
+        default: 'shadow-lg px-3 py-1',
+        sm: 'shadow-lg px-2 py-[2px] text-sm',
         lg: 'shadow-lg h-10 px-8 font-medium text-lg',
         icon: 'h-9 w-9',
       },
