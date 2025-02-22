e2dfd6b16119eb7c9d8f099c77d1a5c2287b6e56
jamie 🌨️ <jamie@wisp.bio>
2025-02-22 16:13:31 -0500

    refactor: edit switch component

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,9 +1,45 @@
 import * as SwitchPrimitive from '@radix-ui/react-switch'
+import { cva, VariantProps } from 'class-variance-authority'
 
-function Switch() {
+import { cn } from '@/lib/utils'
+
+const rootVariants = cva('inline-flex relative justify-center items-center bg-input radix-state-checked:bg-primary/60 shadow rounded-full outline-0 focus-visible:ring-2 transition-all cursor-pointer', {
+  variants: {
+    size: {
+      sm: 'w-10 h-6',
+      md: 'w-14 h-8',
+      lg: 'w-18 h-10',
+    },
+  },
+})
+
+const thumbVariants = cva('block bg-foreground shadow-lg rounded-full transition-all duration-100', {
+  variants: {
+    size: {
+      sm: 'size-5 -translate-x-2 radix-state-checked:translate-x-2',
+      md: 'size-6 -translate-x-3 radix-state-checked:translate-x-3',
+      lg: 'size-8 -translate-x-4 radix-state-checked:translate-x-4',
+    },
+  },
+})
+function Switch({ className, size = 'md', ...props }: SwitchPrimitive.SwitchProps & { size?: VariantProps<typeof rootVariants>['size'] }) {
   return (
-    <SwitchPrimitive.Root className="inline-flex relative justify-center items-center bg-input radix-state-checked:bg-primary/60 shadow-lg rounded-2xl outline-0 focus-visible:ring-2 w-14 h-8 max-h-8 transition-all cursor-pointer">
-      <SwitchPrimitive.Thumb className="block bg-foreground shadow-lg rounded-full size-6 transition-all -translate-x-3 radix-state-checked:translate-x-3 duration-100" />
+    <SwitchPrimitive.Root
+      className={cn(
+        rootVariants({
+          size,
+        }),
+        className,
+      )}
+      {...props}
+    >
+      <SwitchPrimitive.Thumb
+        className={cn(
+          thumbVariants({
+            size,
+          }),
+        )}
+      />
     </SwitchPrimitive.Root>
   )
 }
