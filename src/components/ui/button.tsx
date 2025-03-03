0b2e02824a5ae19b0a4a2d516c806ad11ce5c30b
jamie 🌨️ <jamie@wisp.bio>
2025-03-03 16:24:41 -0500

    refactor: edit button component to use style object

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,32 +4,30 @@ import type { VariantProps } from 'class-variance-authority'
 import * as React from 'react'
 
 import { cn } from '@/lib/utils'
+import { interactiveStyles } from '@/styles/styles'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded transition-all cursor-pointer disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*=\'size-\'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+  [
+    'inline-flex items-center justify-center gap-2 whitespace-nowrap cursor-pointer [&_svg]:pointer-events-none [&_svg:not([class*=\'size-\'])]:size-4 shrink-0 [&_svg]:shrink-0',
+    interactiveStyles.base,
+  ],
   {
     variants: {
       variant: {
-        default:
-          'bg-primary border hover:border-primary-foreground/25 text-primary-foreground shadow active:bg-primary/80 active:border-border',
-        destructive:
-          'bg-destructive/90 hover:bg-destructive active:bg-destructive/80 text-destructive-foreground shadow focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40',
-        outline:
-          'border hover:bg-accent hover:border-primary-foreground/25 active:border-border shadow hover:text-accent-foreground',
-        secondary:
-          'bg-secondary text-secondary-foreground shadow hover:bg-secondary/80',
-        ghost: 'hover:bg-accent active:bg-accent/80 hover:text-accent-foreground',
-        link: 'text-primary-foreground underline-offset-4 hover:underline',
+        primary: interactiveStyles.variant.primary,
+        outline: interactiveStyles.variant.outline,
+        ghost: interactiveStyles.variant.ghost,
+        destructive: interactiveStyles.variant.destructive,
       },
       size: {
-        default: 'h-8 px-3 text-base py-2 has-[>svg]:px-3',
-        sm: 'h-6 text-sm rounded gap-1.5 px-2 has-[>svg]:px-2.5',
-        lg: 'h-10 text-md rounded px-6 has-[>svg]:px-4',
-        icon: 'size-9',
+        default: interactiveStyles.size.default,
+        sm: interactiveStyles.size.sm,
+        lg: interactiveStyles.size.lg,
+        icon: interactiveStyles.size.icon,
       },
     },
     defaultVariants: {
-      variant: 'default',
+      variant: 'primary',
       size: 'default',
     },
   },
@@ -50,11 +48,13 @@ function Button({
   return (
     <Comp
       data-slot="button"
-      className={cn(buttonVariants({
-        variant,
-        size,
+      className={cn(
+        buttonVariants({
+          variant,
+          size,
+        }),
         className,
-      }))}
+      )}
       {...props}
     />
   )
