99e1b2fd443cf35f75d45918ba8ce903414ef015
jamie 🌨️ <jamie@wisp.bio>
2025-03-05 17:20:26 -0500

    refactor: update button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,14 +1,19 @@
-import { Slot } from '@radix-ui/react-slot'
-import { cva } from 'class-variance-authority'
 import type { VariantProps } from 'class-variance-authority'
-import * as React from 'react'
-
 import { cn } from '@/lib/utils'
 import { interactiveStyles } from '@/styles/styles'
+import { Slot } from '@radix-ui/react-slot'
+
+import { cva } from 'class-variance-authority'
+import * as React from 'react'
 
 const buttonVariants = cva(
   [
-    'inline-flex items-center justify-center gap-2 whitespace-nowrap cursor-pointer [&_svg]:pointer-events-none [&_svg:not([class*=\'size-\'])]:size-4 shrink-0 [&_svg]:shrink-0',
+    `
+      inline-flex shrink-0 cursor-pointer items-center justify-center gap-2
+      whitespace-nowrap select-none
+      [&_svg]:pointer-events-none [&_svg]:shrink-0
+      [&_svg:not([class*='size-'])]:size-4
+    `,
     interactiveStyles.base,
   ],
   {
